dbacb20f58164e382c957954d753a29326c87de9
asd12-f <puzanov008@gmail.com>
2026-05-25 23:39:22 +0400

    добавил старые домашки по прогрммированию

---
+++ программирование/task11 (2).cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    int N = 10000;
+    float x, eps;
+    cout << "x="; cin >> x;
+    cout << "eps="; cin >> eps;
+
+    float a = x, S = a;
+
+    cout << left << setw(3) << "i\t" << setw(10) << "a\t";
+    cout << setw(10) << "S" << endl;
+
+    cout << left << setw(3) << 1 << "\t" << setw(10) << a;
+    cout << "\t" << setw(10) << S << endl;
+
+    int i;
+    for (i = 2; fabs(a) > eps && i <= N; i++) {
+        a *= (-x * x) * (2 * i - 3) / (2 * i - 1);
+        S += a;
+        cout << left << setw(3) << i << "\t" << setw(10) << a;
+        cout << "\t" << setw(10) << S << endl;
+    }
+
+    if (i > N && fabs(a) > eps) {
+        cout << "Ряд расходится" << endl;
+    } else {
+        cout << "Сумма ряда = " << S << endl;
+    }
+
+    return 0;
+}
---
+++ программирование/task3 (13).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+
+    int n;
+    float x;
+    cout << "n="; cin >> n;
+    cout << "x="; cin >> x;
+
+    float a = x;
+    //начальные условия
+
+    cout << left << setw(3) << "i\t" << setw(10) << "a" << endl;
+    //заголовок
+
+    cout << left << setw(3) << 1 << "\t" << setw(10) << a << endl;
+    //1 шаг
+
+    for (int i = 2; i <= n; i++) {
+        //i-ый шаг
+        a *= x / i;
+        cout << left << setw(3) << i << "\t" << setw(10) << a << endl;
+    }
+
+    return 0;
+}
